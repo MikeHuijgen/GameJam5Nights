acff1707f41d93cfb9e13e3d2dd6c5b73583db72
Tessa van Elderen <tessa.vanelderen@gmail.com>
2022-09-08 17:05:42 +0200

    CutScene

--- Assets/PlayerCameraStuff/Cutscene.cs
+++ Assets/PlayerCameraStuff/Cutscene.cs
@@ -8,11 +8,6 @@ public class Cutscene : MonoBehaviour
     //Code uitzetten : "outline - colorchangecrosshair - crosshairmovement".
     //Als je op de deur klikt begint de animatie. Niet aan het begin van de game. 
 
-
-    [SerializeField] Outline outline;
-    [SerializeField] ColorChangeCrossHair colorChange;
-    [SerializeField] CrossHairMovement hairMovement;
-
     void Start()
     {
         
